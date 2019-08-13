0a827d092aca37f6a548cc4ce998800227d20e19
AlvaroCastaneda <alvleecas@gmail.com>
2019-08-13 18:46:16 -0500

    added logic for multi player

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const winningCombos = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,4,8],
-    [2,4,6],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
 ];
 
 var singlePlayer = true;
 const grid = () => Array.from(document.getElementsByClassName('q'));
 const qNumId = (qEl) => Number.parseInt(qEl.id.replace('q', ''));
 const emptyQs = () => grid().filter(_qEl => _qEl.innerText === '');
-const allSame = (arr) => arr.every(_qEl => _qEl.innerText === arr[0].innerText && _qEl.innerText !== ''); 
+const allSame = (arr) => arr.every(_qEl => _qEl.innerText === arr[0].innerText && _qEl.innerText !== '');
 
 const takeTurn = (index, letter) => grid()[index].innerText = letter;
 const opponentChoice = () => qNumId(emptyQs()[Math.floor(Math.random() * emptyQs().length)]);
 
-const endGame = (winningSequence) => { 
+const endGame = (winningSequence) => {
     winningSequence.forEach(_qEl => _qEl.classList.add('winner'));
     disableListeners();
 };
@@ -28,7 +28,7 @@ const checkForVictory = () => {
     winningCombos.forEach(_c => {
         const _grid = grid();
         const sequence = [_grid[_c[0]], _grid[_c[1]], _grid[_c[2]]];
-        if(allSame(sequence)) {
+        if (allSame(sequence)) {
             victory = true;
             endGame(sequence);
 
@@ -38,37 +38,45 @@ const checkForVictory = () => {
     return victory;
 };
 
-const checkNumOfPlayers = () => {
-    if(singlePlayer){
-        opponentTurn();
-    } else
-    opponentTurn1();
+const clickFn = ($event) => {
+    disableSecondPlayer();
+    takeTurn(qNumId($event.target), 'x');
+    if (!checkForVictory()){
+        if(singlePlayer){
+            disableListeners();
+            opponentTurn();
+        } else {
+            enableSecondPlayer();
+            disableListeners();
+            secondPlayer();  
+        }
+    }        
 };
 
-const opponentTurn1 = ($event) => {
-    debugger;
-        takeTurn(qNumId($event.target), 'o');
-        if(!checkForVictory())
+const secondPlayer = $event => {
+    takeTurn(qNumId($event.target), "O");
+    if (!checkForVictory()) {
+        enableListeners();
+        disableSecondPlayer();
         clickFn();
-}
+    }
+};
 
 const opponentTurn = () => {
     disableListeners();
     setTimeout(() => {
         takeTurn(opponentChoice(), 'o');
-        if(!checkForVictory())
+        if (!checkForVictory())
             enableListeners();
     }, 1000);
 }
 
-const clickFn = ($event) => {
-    takeTurn(qNumId($event.target), 'x');
-    if(!checkForVictory())
-        // opponentTurn();
-        checkNumOfPlayers();
-};
-
 const enableListeners = () => grid().forEach(_qEl => _qEl.addEventListener('click', clickFn));
 const disableListeners = () => grid().forEach(_qEl => _qEl.removeEventListener('click', clickFn));
 
+const enableSecondPlayer = () => grid().forEach(_qEl => _qEl.addEventListener("click", secondPlayer));
+const disableSecondPlayer = () => grid().forEach(_qEl => _qEl.addEventListener("click", secondPlayer));
+
+
 enableListeners();
+disableSecondPlayer();
